b73daa90ab588efd935f3f9d792f9db1b67d86a8
Rinsho <16826531+Rinsho@users.noreply.github.com>
2021-11-21 01:48:07 -0600

    Add initial ElementGenerator tests.

---
+++ ObjectInflator.Tests/ElementToStringVisitor.cs
@@ -0,0 +1,62 @@
+
+using System;
+using System.Text;
+
+internal class ElementToStringVisitor : IVisitor
+{
+    private StringBuilder _result = new StringBuilder();
+    public string Result { get => _result.ToString(); }
+
+    public void VisitChildren(IElement element)
+    {
+        foreach (IElement child in element.Children)
+            child.Accept(this);
+    }
+
+    public void Visit(Data element)
+    {
+        _result.Append("|Data");
+    }
+
+    public void Visit(Field element)
+    {
+        _result.Append("|Field");
+        VisitChildren(element);
+    }
+
+    public void Visit(Property element)
+    {
+        _result.Append("|Property");
+        VisitChildren(element);
+    }
+
+    public void Visit(Method element)
+    {
+        _result.Append("|Method");
+        VisitChildren(element);
+    }
+
+    public void Visit(PropertyIndex element)
+    {
+        _result.Append("|PropertyIndex");
+        VisitChildren(element);
+    }
+
+    public void Visit(Object element)
+    {
+        _result.Append("|Object");
+        VisitChildren(element);
+    }
+
+    public void Visit(Array element)
+    {
+        _result.Append("|Array");
+        VisitChildren(element);
+    }
+
+    public void Visit(Constructor element)
+    {
+        _result.Append("|Constructor");
+        VisitChildren(element);
+    }
+}
--- ObjectInflator.Tests/ObjectInflatorTests.cs
+++ ObjectInflator.Tests/ObjectInflatorTests.cs
@@ -8,29 +8,124 @@ namespace ExpressionGen.Tests
     [TestFixture]
     public class Tests
     {
-        class Test
+        class NoTargets
         {
-            [DataTarget(DataId = "X")]
-            public int x;
-
-            [DataTarget(DataId = "Y")]
-            public string Y {get; set;}
-
-            private string _z = string.Empty;
-            [DataTarget]
-            public string Z { set => this._z = value; }
-        }
-
-        [SetUp]
-        public void Setup()
-        {
+            public string x;
         }
 
         [Test]
-        public void Test1()
+        public void ElementGenerator_OnNoTargets_CreateData()
         {
-            
-            Assert.Fail();
+            //Arrange
+            Type testType = typeof(NoTargets);
+            ElementToStringVisitor visitor = new ElementToStringVisitor();
+
+            //Act
+            IElement dataElement = ElementGenerator.CreateType(testType);
+            dataElement.Accept(visitor);
+            string result = visitor.Result;
+
+            //Assert
+            string expectedResult = "|Data";
+            Assert.That(string.Equals(result, expectedResult, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [DataTarget(DataId = "X")]
+        class ObjectWithNoSubTargets
+        {
+            public int X { get; set; }
+
+            public ObjectWithNoSubTargets(int x) =>
+                X = x;
+
+            public static implicit operator ObjectWithNoSubTargets(int x) =>
+                new ObjectWithNoSubTargets(x);
+        }
+
+        [Test]
+        public void ElementGenerator_OnObjectWithNoSubTargets_CreateData()
+        {
+            //Arrange
+            Type testType = typeof(ObjectWithNoSubTargets);
+            ElementToStringVisitor visitor = new ElementToStringVisitor();
+
+            //Act
+            IElement dataElement = ElementGenerator.CreateType(testType);           
+            dataElement.Accept(visitor);
+            string result = visitor.Result;
+
+            //Assert
+            string expectedResult = "|Data";
+            Assert.That(string.Equals(result, expectedResult, StringComparison.OrdinalIgnoreCase));
+        }
+
+        class ObjectWithValidField
+        {
+            [DataTarget(DataId = "X")]
+            public int X;
+        }
+
+        [Test]
+        public void ElementGenerator_OnFieldTarget_CreateObject()
+        {
+            //Arrange
+            Type testType = typeof(ObjectWithValidField);
+            ElementToStringVisitor visitor = new ElementToStringVisitor();
+
+            //Act
+            IElement objectElement = ElementGenerator.CreateType(testType);
+            objectElement.Accept(visitor);
+            string result = visitor.Result;
+
+            //Assert
+            string expectedResult = "|Object|Field|Data";
+            Assert.That(string.Equals(result, expectedResult, StringComparison.OrdinalIgnoreCase));
+        }
+
+        class ObjectWithValidProperty
+        {
+            [DataTarget(DataId = "X")]
+            public int X { get; private set; }
+        }
+
+        [Test]
+        public void ElementGenerator_OnPropertyTarget_CreateObject()
+        {
+            //Arrange
+            Type testType = typeof(ObjectWithValidProperty);
+            ElementToStringVisitor visitor = new ElementToStringVisitor();
+
+            //Act
+            IElement objectElement = ElementGenerator.CreateType(testType);
+            objectElement.Accept(visitor);
+            string result = visitor.Result;
+
+            //Assert
+            string expectedResult = "|Object|Property|Data";
+            Assert.That(string.Equals(result, expectedResult, StringComparison.OrdinalIgnoreCase));
+        }
+
+        class ObjectWithValidMethod
+        {
+            private int _x;
+            public void X([DataTarget(DataId = "x")] int x) => _x = x;
+        }
+
+        [Test]
+        public void ElementGenerator_OnMethodTarget_CreateObject()
+        {
+            //Arrange
+            Type testType = typeof(ObjectWithValidMethod);
+            ElementToStringVisitor visitor = new ElementToStringVisitor();
+
+            //Act
+            IElement objectElement = ElementGenerator.CreateType(testType);
+            objectElement.Accept(visitor);
+            string result = visitor.Result;
+
+            //Assert
+            string expectedResult = "|Object|Method|Data";
+            Assert.That(string.Equals(result, expectedResult, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
