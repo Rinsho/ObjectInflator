f5e383eddad2e114cb8adcdd5a3927516a72ae0c
Rinsho <16826531+Rinsho@users.noreply.github.com>
2021-11-25 23:01:19 -0600

    Add DataConverter; fix ConstructionVisitor issues.

---
+++ ObjectInflator/src/DataHandlers/DataConverter.cs
@@ -0,0 +1,61 @@
+
+using System;
+using System.Reflection;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Collections;
+using System.Collections.Generic;
+
+internal class DataConverter
+{
+    protected BindingFlags _operatorFlags = BindingFlags.Public | BindingFlags.Static;
+    protected string _implicitOperatorName = "op_Implicit";
+    protected string _explicitOperatorName = "op_Explicit";
+
+
+    public virtual LambdaExpression GetConverterFor(Type targetType)
+    {
+        ParameterExpression data = Expression.Parameter(typeof(object));
+        return Expression.Lambda(
+            CreateCompileTimeConversionsFor(targetType, data), 
+            data
+        );
+    }
+
+    protected Expression CreateCompileTimeConversionsFor(
+        Type targetType, 
+        ParameterExpression data
+    ) 
+    {
+        //Get all implicit and explicit conversion operators on the
+        //targetType that convert to the targetType using a single parameter.
+        //Create switch cases that test the given data object's run-time type
+        //against these parameter types, converting the data into a compatible
+        //parameter type (unboxes too) before calling the conversion operator.
+        
+        IEnumerable<SwitchCase> cases = 
+            targetType.GetMember(_implicitOperatorName, _operatorFlags)
+            .Concat(targetType.GetMember(_explicitOperatorName, _operatorFlags))
+            .Cast<MethodInfo>()
+            .Where(converter => converter.ReturnType == targetType)
+            .Where(converter => converter.GetParameters().Length == 1)
+            .Select(converter => {
+                Type parameterType = converter.GetParameters()[0].ParameterType;
+                return Expression.SwitchCase(
+                    Expression.Call(null, converter, Expression.Convert(data, parameterType)),
+                    Expression.Constant(parameterType)
+                );
+            });
+        SwitchExpression switchExpression = Expression.Switch(
+            targetType,
+            Expression.Call(
+                data,
+                data.Type.GetMethod(nameof(object.GetType))
+            ),
+            Expression.Convert(data, targetType),
+            null,
+            cases
+        );
+        return switchExpression;
+    } 
+}
--- ObjectInflator/src/Visitors/ConstructionVisitor.cs
+++ ObjectInflator/src/Visitors/ConstructionVisitor.cs
@@ -7,230 +7,258 @@ using System.Reflection;
 internal class ConstructionVisitor : IVisitor
 {
     private DataContext _dataContext;
+    private DataConverter _dataConverter;
     private Stack<Expression> _constructedElements;
     private Stack<ParameterExpression> _parentReferences;
 
-    public ConstructionVisitor(DataContext dataContext)
+    public ConstructionVisitor(DataContext dataContext, DataConverter converter)
     {
         _constructedElements = new Stack<Expression>();
         _parentReferences = new Stack<ParameterExpression>();    
         _dataContext = dataContext;
+        _dataConverter = converter;
     }
     
-    public Func<Dictionary<string, object>, object> GetResult()
+    public Func<T, R> GetResult<T, R>()
     {
-        return Expression.Lambda<Func<Dictionary<string, object>, object>>(
-            _constructedElements.Pop(),
+        ParameterExpression parameter = Expression.Parameter(typeof(T));
+        BlockExpression body =
+            Expression.Block(
+                new[] { _dataContext.GetBaseParameter() },
+                Expression.Assign(
+                    _dataContext.GetBaseParameter(),
+                    Expression.ListInit(
+                        Expression.New(typeof(Dictionary<string, object>)),
+                        Expression.ElementInit(
+                            typeof(IDictionary<string, object>).GetMethod(nameof(IDictionary<string, object>.Add)),
+                            Expression.Constant(string.Empty),
+                            Expression.Convert(parameter, typeof(object))
+                        )
+                    )
+                ),
+                _constructedElements.Pop()
+            );
+        return Expression.Lambda<Func<T, R>>(
+            body,
             false,          //Tail call optimization not applicable.
-            _dataContext.GetBaseParameter()
+            parameter
         ).Compile(false);   //No interpretation if we can fully compile.
     }
 
     //IVisitor interface
+    private void Visit(Element element, Action<Expression> constructionHandler)
+    {
+        Action cleanup = () => {};
+        if (element.HasDataScope)
+        {
+            _dataContext.AddContextUsing(element);
+            cleanup = () => _dataContext.RemoveCurrent();
+        }
+        Expression data = _dataContext.GetCurrent();
+        constructionHandler(data);
+        cleanup();
+    }
+
     public void Visit(Data element)
     {
-        _constructedElements.Push(
-            Expression.ConvertChecked(_dataContext.GetCurrent(), element.Type)
-        );
+        Visit(element, (data) => {
+            _constructedElements.Push(
+                Expression.Invoke(
+                    _dataConverter.GetConverterFor(element.Type),
+                    data
+                )
+            );
+        });
     }
 
     public void Visit(Array element)
     {
-        //Get current data context and let this element filter it
-        Expression data = _dataContext.AddContext(element);
-        if (!data.Type.IsArray)
-            throw new InvalidDataException("Array member requires array-type data.");
+        Visit(element, (data) => {
+            if (!data.Type.IsArray)
+                throw new InvalidDataException("Array member requires array-type data.");
 
-        //Visit array's child
-        element.ArrayObject.Accept(this);
+            //Visit array's child
+            element.ArrayObject.Accept(this);
 
-        //Construct array element
-        ParameterExpression array = Expression.Parameter(element.Type);
-        Expression arrayElement = Expression.Block(
-            array.Type,
-            new[] { array },
-            Expression.Assign(
-                array, 
-                Expression.NewArrayBounds(
-                    array.Type.GetElementType(),
-                    Expression.MakeMemberAccess(
-                        data,
-                        typeof(Array).GetMember("Length")[0]
-                    )
-                )
-            ),
-            element.Iterator.Create(
-                data,
+            //Construct array element
+            ParameterExpression array = Expression.Parameter(element.Type);
+            Expression arrayElement = Expression.Block(
+                array.Type,
+                new[] { array },
                 Expression.Assign(
-                    Expression.ArrayAccess(
-                        array, 
-                        new[] { element.Iterator.InnerIterator }
-                    ),
-                    _constructedElements.Pop()
-                )
-            ),
-            array
-        );
+                    array, 
+                    Expression.NewArrayBounds(
+                        array.Type.GetElementType(),
+                        Expression.MakeMemberAccess(
+                            data,
+                            typeof(Array).GetMember("Length")[0]
+                        )
+                    )
+                ),
+                element.Iterator.Create(
+                    data,
+                    Expression.Assign(
+                        Expression.ArrayAccess(
+                            array, 
+                            new[] { element.Iterator.InnerIterator }
+                        ),
+                        _constructedElements.Pop()
+                    )
+                ),
+                array
+            );
 
-        //Push constructed array element for parent
-        _constructedElements.Push(arrayElement);
-        //Remove data context for this element
-        _dataContext.RemoveCurrent();
+            //Push constructed array element for parent
+            _constructedElements.Push(arrayElement);
+        });
     }
     
     public void Visit(Field element)
     {
-        Expression data = _dataContext.AddContext(element);
+        Visit(element, (data) => {
+            //Visit field's child
+            element.FieldObject.Accept(this);
 
-        //Visit field's child
-        element.FieldObject.Accept(this);
+            //Construct field
+            Expression fieldElement = Expression.Assign(
+                Expression.MakeMemberAccess(
+                    _parentReferences.Peek(), 
+                    element.FieldInfo
+                ), 
+                _constructedElements.Pop()
+            );
 
-        //Construct field
-        Expression fieldElement = Expression.Assign(
-            Expression.MakeMemberAccess(
-                _parentReferences.Peek(), 
-                element.FieldInfo
-            ), 
-            _constructedElements.Pop()
-        );
-
-        //Push field element for parent
-        _constructedElements.Push(fieldElement);
-
-        _dataContext.RemoveCurrent();
+            //Push field element for parent
+            _constructedElements.Push(fieldElement);
+        });
     }
     
     public void Visit(Property element)
     {
-        Expression data = _dataContext.AddContext(element);
+        Visit(element, (data) => {
+            //Visit property's child
+            element.PropertyObject.Accept(this);
 
-        //Visit property's child
-        element.PropertyObject.Accept(this);
+            //Construct property
+            Expression propertyElement = Expression.Assign(
+                Expression.MakeMemberAccess(
+                    _parentReferences.Peek(), 
+                    element.PropertyInfo), 
+                _constructedElements.Pop()
+            );
 
-        //Construct property
-        Expression propertyElement = Expression.Assign(
-            Expression.MakeMemberAccess(
-                _parentReferences.Peek(), 
-                element.PropertyInfo), 
-            _constructedElements.Pop()
-        );
-
-        //Push property element for parent
-        _constructedElements.Push(propertyElement);
-
-        _dataContext.RemoveCurrent();
+            //Push property element for parent
+            _constructedElements.Push(propertyElement);
+        });
     }
     
     public void Visit(Method element)
     {
-        Expression data = _dataContext.AddContext(element);
+        Visit(element, (data) => {
+            //Visit method's children
+            List<Expression> parameters = new List<Expression>();
+            foreach (Element child in element.ParameterObjects)
+            {
+                child.Accept(this);
+                parameters.Add(_constructedElements.Pop());
+            }
 
-        //Visit method's children
-        List<Expression> parameters = new List<Expression>();
-        foreach (IElement child in element.ParameterObjects)
-        {
-            child.Accept(this);
-            parameters.Add(_constructedElements.Pop());
-        }
+            //Construct method
+            Expression methodElement = Expression.Call(
+                _parentReferences.Peek(), 
+                element.MethodInfo,
+                parameters
+            );
 
-        //Construct method
-        Expression methodElement = Expression.Call(
-            _parentReferences.Peek(), 
-            element.MethodInfo,
-            parameters
-        );
-
-        //Push method element for parent
-        _constructedElements.Push(methodElement);
-
-        _dataContext.RemoveCurrent();
+            //Push method element for parent
+            _constructedElements.Push(methodElement);
+        });
     }
 
     public void Visit(Object element)
     {
-        Expression data = _dataContext.AddContext(element);
+        Visit(element, (data) => {
+            //Create parent reference
+            ParameterExpression objectReference = Expression.Parameter(element.Type);
+            _parentReferences.Push(objectReference);
 
-        //Create parent reference
-        ParameterExpression objectReference = Expression.Parameter(element.Type);
-        _parentReferences.Push(objectReference);
+            List<Expression> members = new List<Expression>();
 
-        List<Expression> members = new List<Expression>();
+            //Visit object's constructor
+            element.Constructor.Accept(this);
+            members.Add(
+                Expression.Assign(
+                    objectReference,
+                    _constructedElements.Pop()
+                )
+            );
 
-        //Visit object's constructor
-        element.Constructor.Accept(this);
-        members.Add(_constructedElements.Pop());
+            //Visit object's members 
+            foreach (Element child in element.Members)
+            {
+                child.Accept(this);
+                members.Add(_constructedElements.Pop());
+            }
 
-        //Visit object's members 
-        foreach (IElement child in element.Members)
-        {
-            child.Accept(this);
-            members.Add(_constructedElements.Pop());
-        }
+            //Add object reference as return
+            members.Add(objectReference);
 
-        //Add object reference as return
-        members.Add(objectReference);
+            //Construct object
+            Expression objectElement = Expression.Block(
+                element.Type,
+                new[] { objectReference },
+                members
+            );
 
-        //Construct object
-        Expression objectElement = Expression.Block(
-            element.Type,
-            new[] { objectReference },
-            members
-        );
-
-        //Push object element for parent
-        _constructedElements.Push(objectElement);
-
-        _dataContext.RemoveCurrent();
-        _parentReferences.Pop();
+            //Push object element for parent
+            _constructedElements.Push(objectElement);
+            _parentReferences.Pop();
+        });
     }
 
     public void Visit(PropertyIndex element)
     {
-        Expression data = _dataContext.AddContext(element);
+        Visit(element, (data) => {
+            //Visit property index's child
+            element.PropertyObject.Accept(this);
 
-        //Visit property index's child
-        element.PropertyObject.Accept(this);
+            //Construct property index
+            Expression propertyIndexElement = element.Iterator.Create(
+                data,
+                Expression.Assign(
+                    Expression.MakeIndex(
+                        _parentReferences.Peek(), 
+                        element.PropertyInfo, 
+                        new[] { element.Iterator.InnerIterator }
+                    ),
+                    _constructedElements.Pop()
+                )
+            );
 
-        //Construct property index
-        Expression propertyIndexElement = element.Iterator.Create(
-            data,
-            Expression.Assign(
-                Expression.MakeIndex(
-                    _parentReferences.Peek(), 
-                    element.PropertyInfo, 
-                    new[] { element.Iterator.InnerIterator }
-                ),
-                _constructedElements.Pop()
-            )
-        );
-
-        //Push property index element for parent
-        _constructedElements.Push(propertyIndexElement);
-
-        _dataContext.RemoveCurrent();
+            //Push property index element for parent
+            _constructedElements.Push(propertyIndexElement);
+        });
     }
 
     public void Visit(Constructor element)
     {
-        Expression data = _dataContext.AddContext(element);
+        Visit(element, (data) => {
+            //Visit constructor's children
+            List<Expression> parameters = new List<Expression>();
+            foreach (Element child in element.ParameterObjects)
+            {
+                child.Accept(this);
+                parameters.Add(_constructedElements.Pop());
+            }
 
-        //Visit constructor's children
-        List<Expression> parameters = new List<Expression>();
-        foreach (IElement child in element.ParameterObjects)
-        {
-            child.Accept(this);
-            parameters.Add(_constructedElements.Pop());
-        }
+            //Construct constructor
+            Expression constructorElement = Expression.New(
+                element.ConstructorInfo, 
+                parameters
+            );
 
-        //Construct constructor
-        Expression constructorElement = Expression.New(
-            element.ConstructorInfo, 
-            parameters
-        );
-
-        //Push constructor element for parent
-        _constructedElements.Push(constructorElement);
-
-        _dataContext.RemoveCurrent();
+            //Push constructor element for parent
+            _constructedElements.Push(constructorElement);
+        });
     }
 }
